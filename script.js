b92840e147e175f4be07a06ad64cbba6d7f1d98a
ostachim <44241143+ostachim@users.noreply.github.com>
2020-05-09 13:12:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".button1");
+const burger = document.querySelector(".burger");
+burger.addEventListener("click",button1)
+
+function button1(){
+    const asd = document.querySelector("aside");
+    btn.classList.toggle("active");
+    asd.classList.toggle("active");
+    
+}
